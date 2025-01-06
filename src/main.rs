c0b8b3446125561f8c8fc23524b80b5236ef6748
melizimar <melizimar@gmail.com>
2025-01-06 14:00:33 -0300

    :sparkles: feat: Adicionando rotas do caminho Users e implementando os metodos inicias do CRUD

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,19 @@
 use axum::{
-    http::StatusCode, response::IntoResponse, routing::get, Router
+    http::StatusCode,
+    response::IntoResponse,
+    routing::{delete, get, post, put},
+    Router,
 };
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(hello));
+    let app = Router::new()
+        .route("/", get(hello))
+        .route("/users", get(get_users))
+        .route("/users/:id", get(get_user_by_id))
+        .route("/users", post(create_user))
+        .route("/users", put(update_user))
+        .route("/users", delete(delete_user));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
@@ -12,4 +21,24 @@ async fn main() {
 
 async fn hello() -> impl IntoResponse {
     (StatusCode::OK, "Hello, World!")
+}
+
+async fn get_users() -> impl IntoResponse {
+    (StatusCode::OK, "Lista com todos os usuários!")
+}
+
+async fn get_user_by_id() -> impl IntoResponse {
+    (StatusCode::OK, "User: id")
+}
+
+async fn create_user() -> impl IntoResponse {
+    (StatusCode::CREATED, "Created")
+}
+
+async fn update_user() -> impl IntoResponse {
+    (StatusCode::OK, "Updated")
+}
+
+async fn delete_user() -> impl IntoResponse {
+    (StatusCode::OK, "deleted")
 }
