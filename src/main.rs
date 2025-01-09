6a7611b1308ff12d82b8b454d7bece514a5c9639
melizimar <melizimar@gmail.com>
2025-01-09 15:03:01 -0300

    :construction: Adicionando importação em massa de usuário a partir de arquivos csv

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,14 @@ mod user;
 
 use std::{collections::HashMap, sync::Arc};
 
+use std::io::{self, Write};
+use std::path;
+
+use axum::extract::multipart::Field;
 use serde_json::json;
 use time::macros::date;
+use tokio::fs;
+use tokio::io::AsyncWriteExt;
 use tokio::sync::RwLock;
 use user::{User, UserDTO};
 use uuid::Uuid;
@@ -14,7 +20,7 @@ use tower::ServiceExt;
 
 use axum::{
     body::Body,
-    extract::{Json, Path, State},
+    extract::{Json, Multipart, Path, State},
     http::{Request, StatusCode},
     response::IntoResponse,
     routing::{delete, get, post, put},
@@ -33,7 +39,7 @@ async fn main() {
     users.insert(user1.id, user1.clone());
     users.insert(user2.id, user2.clone());
 
-    let shared_state = Arc::new(RwLock::new(users));
+    let shared_state_users = Arc::new(RwLock::new(users));
 
     let app = Router::new()
         .route("/", get(hello))
@@ -41,8 +47,9 @@ async fn main() {
         .route("/users/{uuid}", get(get_user_by_id))
         .route("/users", post(create_user))
         .route("/users", put(update_user))
+        .route("/users/import", post(import_users_by_csv))
         .route("/users/{uuid}", delete(delete_user))
-        .with_state(shared_state);
+        .with_state(shared_state_users);
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
@@ -100,6 +107,49 @@ async fn update_user() -> impl IntoResponse {
     (StatusCode::OK, "Updated")
 }
 
+async fn import_users_by_csv(mut multipart: Multipart) -> impl IntoResponse {
+    while let Some(field) = multipart
+        .next_field()
+        .await
+        .map_err(|_| StatusCode::BAD_REQUEST)
+        .unwrap()
+    {
+        let name = field.name().unwrap().to_string();
+        let file_name = field.file_name().unwrap().to_string();
+        let file_extension = match path::Path::new(&file_name).extension() {
+            Some(ext) => format!("{}", ext.to_str().unwrap()),
+            None => format!(""),
+        };
+
+        println!(
+            "----------------\nname: {}\nfile_name: {}\nfile_extension: {}",
+            name, file_name, file_extension
+        );
+
+        if name == "csv".to_string() && file_extension == "csv".to_string() {
+            let output = format!(
+                "./src/public/uploads/import/users/{}-{}",
+                uuid::Uuid::now_v7(),
+                file_name.clone()
+            );
+
+            let path = path::Path::new(&output);
+
+            // Verifica se o caminho do arquivo é válido e possui um diretório pai
+            if let Some(parent) = path.parent() {
+                // Garante que os diretórios pai existem
+                fs::create_dir_all(parent).await.unwrap();
+            }
+
+            let data = field.bytes().await.unwrap();
+            let mut file = fs::File::create(path).await.unwrap();
+            file.write(&data).await.unwrap();
+        }
+    }
+
+    StatusCode::ACCEPTED
+}
+
 async fn delete_user(
     State(users): State<AppState>,
     Path(user_uuid): Path<Uuid>,
@@ -123,12 +173,12 @@ async fn test_get_users_route() {
     users.insert(user1.id, user1.clone());
     users.insert(user2.id, user2.clone());
 
-    let shared_state = Arc::new(RwLock::new(users));
+    let shared_state_users = Arc::new(RwLock::new(users));
 
     // Criar o router
     let app = Router::new()
         .route("/users", get(get_users))
-        .with_state(shared_state);
+        .with_state(shared_state_users);
 
     // Simular requisição GET para a rota /users
     let request = Request::builder()
@@ -153,12 +203,12 @@ async fn test_delete_user_route() {
     users.insert(user1.id, user1.clone());
     users.insert(user2.id, user2.clone());
 
-    let shared_state = Arc::new(RwLock::new(users));
+    let shared_state_users = Arc::new(RwLock::new(users));
 
     // Criar o router
     let app = Router::new()
         .route("/users/{uuid}", delete(delete_user))
-        .with_state(shared_state);
+        .with_state(shared_state_users);
 
     let uri = format!("/users/{}", user1.id);
     // Simular requisição GET para a rota /users
